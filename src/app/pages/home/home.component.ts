1d306ec0eef8f0fb861817b4d444c710ed29815d
Priscila Sartori <priscilasartori2@gmail.com>
2023-10-02 00:33:55 -0300

    feat: projetos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,8 +6,32 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  words: string[] = ['Desenvolvedora Web.', 'Desenvolvedora Front-End.', 'Desenvolvedora Back-End.', 'Desenvolvedora Full-Stack.'];
+  currentWordIndex = 0;
+  partialWord = '';
 
-  constructor(private router: Router) { }
+  ngOnInit() {
+    this.typeWord();
+  }
+  
+  typeWord() {
+  const lastWord = this.words[this.currentWordIndex];
+  const length = lastWord.length;
 
+  let index = 0;
+  const intervalId = setInterval(() => {
+    if (index < length) {
+      this.partialWord += lastWord[index];
+      index+=1;
+    } else {
+      this.partialWord = this.partialWord.slice(0, -1); 
+    }
+    if (this.partialWord.length === 0 && index === length) {
+      clearInterval(intervalId);
+      this.currentWordIndex = (this.currentWordIndex + 1) % this.words.length;
+      setTimeout(() => this.typeWord(), 1500);
+    }
+    }, 100);
+  }
 }
