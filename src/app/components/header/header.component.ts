1afeb557e57f433f54bd4599e8dcc9a2c17202d0
Priscila Sartori <priscilasartori2@gmail.com>
2023-12-29 21:25:47 -0300

    fix: span header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
@@ -6,8 +6,52 @@ import { Router, NavigationEnd } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  currentRoute: string = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private renderer: Renderer2) {}
 
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = this.router.url;
+        console.log('Rota Atual:', this.currentRoute);
+        this.updateHeaderNavSpanPosition();
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.updateHeaderNavSpanPosition();
+  }
+
+  updateHeaderNavSpanPosition() {
+    const span = document.querySelector('.header-nav-span');
+    if (span) {
+      switch (this.currentRoute) {
+        case '/about':
+          this.setSpanPosition(span, '160px', '95px');
+          break;
+        case '/skills':
+          this.setSpanPosition(span, '270px', '130px');
+          break;
+        case '/experience':
+          this.setSpanPosition(span, '430px', '130px');
+          break;
+        case '/education':
+          this.setSpanPosition(span, '590px', '110px');
+          break;
+        case '/projects':
+          this.setSpanPosition(span, '730px', '95px');
+          break;
+        default:
+          this.setSpanPosition(span, '65px', '95px');
+      }
+    }
+  }
+
+  setSpanPosition(span: any, left: string, width: string) {
+    this.renderer.setStyle(span, 'left', left);
+    this.renderer.setStyle(span, 'width', width);
+  }
 }
